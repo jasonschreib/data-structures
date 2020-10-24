3dc33dfdadfac0958e9e195e89b216b1e34b3c5b
Shane Sutro <shanesutro@gmail.com>
2020-10-24 14:18:36 -0600

    Begin LinkedList functional class

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,11 +2,32 @@ var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
+  list.listSize = 0;
 
   list.addToTail = function(value) {
+    var newNode = Node(value);
+    //if list is empty, set list.head & list.tail to this
+    if (!list.head && !list.tail) {
+      list.head = newNode;
+      list.tail = newNode;
+    } else {
+      list.tail = newNode;
+      for (node in list) {
+        var size = _.size(list);
+        console.log(size);
+        if (node.next === null && size === list.listSize) {
+          node.next = newNode;
+        }
+        list.listSize++;
+      }
+    }
   };
 
   list.removeHead = function() {
+    var secondItem = list.head.next;
+    console.log(secondItem);
+    delete list.head;
+    list.head = secondItem;
   };
 
   list.contains = function(target) {
@@ -22,7 +43,6 @@ var Node = function(value) {
   node.next = null;
 
   return node;
-  //minor change
 };
 
 /*
